0f7bcbc6cca8049d983c37356d8850b45b8b8527
atharvapDexTech <atharvap@dexterrtech.com>
2025-10-29 22:07:46 +0530

    Added the api url from render

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,9 @@
 
 // const API_BASE_URL = 'http://localhost:3000/api';
 
-const API_BASE_URL = 'https://instavision-backend.vercel.app/api';
+// const API_BASE_URL = 'https://instavision-backend.vercel.app/api';
+
+const API_BASE_URL = 'https://instavision-backend.onrender.com/api';
 
 export default API_BASE_URL;
 
